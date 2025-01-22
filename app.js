f9bebf027e8e2484a2b326aa8c649d338b893a36
Vanessa Pereira <84781224+vanessappereira@users.noreply.github.com>
2025-01-22 22:18:32 +0000

    minor update

--- app.js
+++
@@ -1,9 +0,0 @@
-require('dotenv').config()
-require("./database/database.js").connect()
-
-const express = require('express')
-const app = express()
-
-app.get("/", (req, res )=>{
-    res.send("<h1>Server is working </h1>")
-})
